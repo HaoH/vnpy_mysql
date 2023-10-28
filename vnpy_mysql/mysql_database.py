141e8c15c069eff59774a0bcb1a0527a4c4d3142
Hao Hu <haohu.th@gmail.com>
2023-10-28 11:07:32 +0800

    add dbresult and index data

--- vnpy_mysql/mysql_database.py
+++ vnpy_mysql/mysql_database.py
@@ -16,7 +16,7 @@ from peewee import (
     fn,
     DoubleField,
     DateField,
-    BooleanField
+    BooleanField, BigIntegerField
 )
 from playhouse.shortcuts import ReconnectMixin
 
@@ -30,7 +30,8 @@ from vnpy.trader.database import (
     convert_tz
 )
 from vnpy.trader.setting import SETTINGS
-from ex_vnpy.object import BasicStockData
+from ex_vnpy.object import BasicStockData, BasicIndexData
+from ex_vnpy.trade_plan import StoplossReason
 
 
 class ReconnectMySQLDatabase(ReconnectMixin, PeeweeMySQLDatabase):
@@ -56,6 +57,29 @@ class DateTimeMillisecondField(DateTimeField):
 
 class DbBarData(Model):
     """K线数据表映射对象"""
+
+    id: AutoField = AutoField()
+
+    symbol: str = CharField()
+    exchange: str = CharField()
+    datetime: datetime = DateTimeField()
+    interval: str = CharField()
+
+    volume: float = DoubleField()
+    turnover: float = DoubleField()
+    open_interest: float = DoubleField()
+    open_price: float = DoubleField()
+    high_price: float = DoubleField()
+    low_price: float = DoubleField()
+    close_price: float = DoubleField()
+
+    class Meta:
+        database: PeeweeMySQLDatabase = db
+        indexes: tuple = ((("symbol", "exchange", "interval", "datetime"), True),)
+
+
+class DbIndexBarData(Model):
+    """指数的K线数据表映射对象"""
 
     id: AutoField = AutoField()
 
@@ -139,13 +163,14 @@ class DbBarOverview(Model):
     symbol: str = CharField()
     exchange: str = CharField()
     interval: str = CharField()
+    type: int = CharField()
     count: int = IntegerField()
     start: datetime = DateTimeField()
     end: datetime = DateTimeField()
 
     class Meta:
         database: PeeweeMySQLDatabase = db
-        indexes: tuple = ((("symbol", "exchange", "interval"), True),)
+        indexes: tuple = ((("symbol", "exchange", "interval", "type"), True),)
 
 
 class DbTickOverview(Model):
@@ -228,6 +253,127 @@ class DbBasicStockData(Model):
         return super(DbBasicStockData, self).save(*args, **kwargs)
 
 
+class DbBasicIndexData(Model):
+    """Index列表映射对象"""
+
+    id = AutoField()
+
+    symbol: str = CharField()
+    name: str = CharField()
+    full_name: str = CharField()
+    exchange: str = CharField()
+    market: str = CharField(default='CN')
+
+    volume: int = BigIntegerField(null=True)
+    turnover: int = BigIntegerField(null=True)
+
+    publish_date: date = DateField(null=True)
+    exit_date: date = DateField(null=True)
+    has_price: bool = BooleanField(default=True)
+    has_weight: bool = BooleanField(default=True)
+    has_components: bool = BooleanField(default=True)
+
+    class Meta:
+        database: PeeweeMySQLDatabase = db
+        indexes = ((("symbol", "exchange", "market"), True),)
+
+
+class DbBacktestingResults(Model):
+    """股票回测结果"""
+
+    id = AutoField()
+    symbol: str = CharField()
+    name: str = CharField()
+    exchange: str = CharField()
+    market: str = CharField()
+
+    backtesting_dt: date = DateField()
+    update_dt: datetime = DateTimeField()
+
+    start_date: datetime = DateTimeField()
+    end_date: datetime = DateTimeField()
+    total_days: int = IntegerField()
+    profit_days: int = IntegerField()
+    loss_days: int = IntegerField()
+    capital: float = DoubleField()
+    end_balance: float = DoubleField()
+    max_drawdown: float = DoubleField()
+    max_ddpercent: float = DoubleField()
+    max_drawdown_duration: int = IntegerField()
+    total_net_pnl: float = DoubleField()
+    daily_net_pnl: float = DoubleField()
+    total_commission: float = DoubleField()
+    daily_commission: float = DoubleField()
+    total_slippage: float = DoubleField()
+    daily_slippage: float = DoubleField()
+    total_turnover: float = DoubleField()
+    daily_turnover: float = DoubleField()
+    total_trade_count: int = IntegerField()
+    daily_trade_count: float = DoubleField()
+    total_return: float = DoubleField()
+    annual_return: float = DoubleField()
+    daily_return: float = DoubleField()
+    return_std: float = DoubleField()
+    sharpe_ratio: float = DoubleField()
+    return_drawdown_ratio: float = DoubleField()
+    win_rate_weighted: float = DoubleField()
+    loss_rate_weighted: float = DoubleField()
+    win_rate_normal: float = DoubleField()
+    total_entry_count: int = IntegerField()
+    entry_win_count: int = IntegerField()
+    entry_loss_count: int = IntegerField()
+    win_count_8: int = IntegerField()
+    win_count_16: int = IntegerField()
+    win_count_16a: int = IntegerField()
+    loss_count_2: int = IntegerField()
+    loss_count_5: int = IntegerField()
+    loss_count_8: int = IntegerField()
+    loss_count_8a: int = IntegerField()
+    total_has_sl_count: int = IntegerField()
+    total_do_sl_count: int = IntegerField()
+    has_sl_Empty: int = IntegerField()
+    has_sl_Init: int = IntegerField()
+    has_sl_Detector: int = IntegerField()
+    has_sl_Dynamic: int = IntegerField()
+    has_sl_LowTwo: int = IntegerField()
+    has_sl_LowFive: int = IntegerField()
+    has_sl_EnterTwo: int = IntegerField()
+    has_sl_Impulse: int = IntegerField()
+    has_sl_Ema: int = IntegerField()
+    has_sl_LargeUp: int = IntegerField()
+    has_sl_LargeVolume: int = IntegerField()
+    has_sl_Engine: int = IntegerField()
+    has_sl_LostSpeed: int = IntegerField()
+    has_sl_LevelLargeUp: int = IntegerField()
+    has_sl_LargeDrop: int = IntegerField()
+    has_sl_LostMovement: int = IntegerField()
+    has_sl_TopPivot: int = IntegerField()
+    do_sl_Init: int = IntegerField()
+    do_sl_Detector: int = IntegerField()
+    do_sl_Dynamic: int = IntegerField()
+    do_sl_LowTwo: int = IntegerField()
+    do_sl_LowFive: int = IntegerField()
+    do_sl_EnterTwo: int = IntegerField()
+    do_sl_Impulse: int = IntegerField()
+    do_sl_Ema: int = IntegerField()
+    do_sl_LargeUp: int = IntegerField()
+    do_sl_LargeVolume: int = IntegerField()
+    do_sl_Engine: int = IntegerField()
+    do_sl_LostSpeed: int = IntegerField()
+    do_sl_LevelLargeUp: int = IntegerField()
+    do_sl_LargeDrop: int = IntegerField()
+    do_sl_LostMovement: int = IntegerField()
+    do_sl_TopPivot: int = IntegerField()
+
+    class Meta:
+        database: PeeweeMySQLDatabase = db
+        indexes = ((("symbol", "exchange", "market", "backtesting_dt"), True),)
+
+    def save(self, *args, **kwargs):
+        self.update_dt = datetime.now()
+        return super(DbBacktestingResults, self).save(*args, **kwargs)
+
+
 class MysqlDatabase(BaseDatabase):
     """Mysql数据库接口"""
 
@@ -236,6 +382,67 @@ class MysqlDatabase(BaseDatabase):
         self.db: PeeweeMySQLDatabase = db
         self.db.connect()
         self.db.create_tables([DbBarData, DbTickData, DbBarOverview, DbTickOverview])
+
+    def save_index_bar_data(self, bars: List[BarData], stream: bool = False) -> bool:
+        """保存Index K线数据"""
+        # 读取主键参数
+        bar: BarData = bars[0]
+        symbol: str = bar.symbol
+        exchange: Exchange = bar.exchange
+        interval: Interval = bar.interval
+
+        # 将BarData数据转换为字典，并调整时区
+        data: list = []
+
+        for bar in bars:
+            bar.datetime = convert_tz(bar.datetime)
+
+            d: dict = bar.__dict__
+            d["exchange"] = d["exchange"].value
+            d["interval"] = d["interval"].value
+            d.pop("gateway_name")
+            d.pop("vt_symbol")
+            data.append(d)
+
+        # 使用upsert操作将数据更新到数据库中
+        with self.db.atomic():
+            for c in chunked(data, 50):
+                DbIndexBarData.insert_many(c).on_conflict_replace().execute()
+
+        # 更新K线汇总数据
+        overview: DbBarOverview = DbBarOverview.get_or_none(
+            DbBarOverview.symbol == symbol,
+            DbBarOverview.exchange == exchange.value,
+            DbBarOverview.interval == interval.value,
+            DbBarOverview.type == "INDX",
+            )
+
+        if not overview:
+            overview: DbBarOverview = DbBarOverview()
+            overview.symbol = symbol
+            overview.exchange = exchange.value
+            overview.interval = interval.value
+            overview.start = bars[0].datetime
+            overview.end = bars[-1].datetime
+            overview.type = "INDX"
+            overview.count = len(bars)
+        elif stream:
+            overview.end = bars[-1].datetime
+            overview.count += len(bars)
+        else:
+            overview.start = min(bars[0].datetime, overview.start)
+            overview.end = max(bars[-1].datetime, overview.end)
+
+            s: ModelSelect = DbIndexBarData.select().where(
+                (DbIndexBarData.symbol == symbol)
+                & (DbIndexBarData.exchange == exchange.value)
+                & (DbIndexBarData.interval == interval.value)
+            )
+            overview.count = s.count()
+
+        overview.save()
+
+        return True
 
     def save_bar_data(self, bars: List[BarData], stream: bool = False) -> bool:
         """保存K线数据"""
@@ -268,6 +475,7 @@ class MysqlDatabase(BaseDatabase):
             DbBarOverview.symbol == symbol,
             DbBarOverview.exchange == exchange.value,
             DbBarOverview.interval == interval.value,
+            DbBarOverview.type == "CS",
         )
 
         if not overview:
@@ -277,6 +485,7 @@ class MysqlDatabase(BaseDatabase):
             overview.interval = interval.value
             overview.start = bars[0].datetime
             overview.end = bars[-1].datetime
+            overview.type = "CS"
             overview.count = len(bars)
         elif stream:
             overview.end = bars[-1].datetime
@@ -367,6 +576,45 @@ class MysqlDatabase(BaseDatabase):
                 & (DbBarData.datetime >= start)
                 & (DbBarData.datetime <= end)
             ).order_by(DbBarData.datetime)
+        )
+
+        bars: List[BarData] = []
+        for db_bar in s:
+            bar: BarData = BarData(
+                symbol=db_bar.symbol,
+                exchange=Exchange(db_bar.exchange),
+                datetime=datetime.fromtimestamp(db_bar.datetime.timestamp(), DB_TZ),
+                interval=Interval(db_bar.interval),
+                volume=db_bar.volume,
+                turnover=db_bar.turnover,
+                open_interest=db_bar.open_interest,
+                open_price=db_bar.open_price,
+                high_price=db_bar.high_price,
+                low_price=db_bar.low_price,
+                close_price=db_bar.close_price,
+                gateway_name="DB"
+            )
+            bars.append(bar)
+
+        return bars
+
+    def load_index_bar_data(
+            self,
+            symbol: str,
+            exchange: Exchange,
+            interval: Interval,
+            start: datetime,
+            end: datetime
+    ) -> List[BarData]:
+        """"""
+        s: ModelSelect = (
+            DbIndexBarData.select().where(
+                (DbIndexBarData.symbol == symbol)
+                & (DbIndexBarData.exchange == exchange.value)
+                & (DbIndexBarData.interval == interval.value)
+                & (DbIndexBarData.datetime >= start)
+                & (DbIndexBarData.datetime <= end)
+            ).order_by(DbIndexBarData.datetime)
         )
 
         bars: List[BarData] = []
@@ -495,15 +743,18 @@ class MysqlDatabase(BaseDatabase):
         d2.execute()
         return count
 
-    def get_bar_overview(self) -> List[BarOverview]:
+    def get_bar_overview(self, type: str = "CS") -> List[BarOverview]:
         """查询数据库中的K线汇总信息"""
         # 如果已有K线，但缺失汇总信息，则执行初始化
-        data_count: int = DbBarData.select().count()
-        overview_count: int = DbBarOverview.select().count()
+        data_count: int = DbBarData.select().count() if type == "CS" else DbIndexBarData.select().count()
+        overview_count: int = DbBarOverview.select().where((DbBarOverview.type == type)).count()
         if data_count and not overview_count:
-            self.init_bar_overview()
+            if type == "CS":
+                self.init_bar_overview()
+            else:
+                self.init_index_bar_overview()
 
-        s: ModelSelect = DbBarOverview.select()
+        s: ModelSelect = DbBarOverview.select().where((DbBarOverview.type == type))
         overviews: List[BarOverview] = []
         for overview in s:
             overview.exchange = Exchange(overview.exchange)
@@ -542,28 +793,77 @@ class MysqlDatabase(BaseDatabase):
             overview.symbol = data.symbol
             overview.exchange = data.exchange
             overview.interval = data.interval
+            overview.type = "CS"
             overview.count = data.count
 
             start_bar: DbBarData = (
                 DbBarData.select()
-                    .where(
+                .where(
                     (DbBarData.symbol == data.symbol)
                     & (DbBarData.exchange == data.exchange)
                     & (DbBarData.interval == data.interval)
                 )
-                    .order_by(DbBarData.datetime.asc())
-                    .first()
+                .order_by(DbBarData.datetime.asc())
+                .first()
             )
             overview.start = start_bar.datetime
 
             end_bar: DbBarData = (
                 DbBarData.select()
-                    .where(
+                .where(
                     (DbBarData.symbol == data.symbol)
                     & (DbBarData.exchange == data.exchange)
                     & (DbBarData.interval == data.interval)
                 )
-                    .order_by(DbBarData.datetime.desc())
+                .order_by(DbBarData.datetime.desc())
+                .first()
+            )
+            overview.end = end_bar.datetime
+
+            overview.save()
+    def init_index_bar_overview(self) -> None:
+        """初始化数据库中的K线汇总信息"""
+        s: ModelSelect = (
+            DbIndexBarData.select(
+                DbIndexBarData.symbol,
+                DbIndexBarData.exchange,
+                DbIndexBarData.interval,
+                fn.COUNT(DbIndexBarData.id).alias("count")
+            ).group_by(
+                DbIndexBarData.symbol,
+                DbIndexBarData.exchange,
+                DbIndexBarData.interval
+            )
+        )
+
+        for data in s:
+            overview: DbBarOverview = DbBarOverview()
+            overview.symbol = data.symbol
+            overview.exchange = data.exchange
+            overview.interval = data.interval
+            overview.type = "INDX"
+            overview.count = data.count
+
+            start_bar: DbIndexBarData = (
+                DbIndexBarData.select()
+                    .where(
+                    (DbIndexBarData.symbol == data.symbol)
+                    & (DbIndexBarData.exchange == data.exchange)
+                    & (DbIndexBarData.interval == data.interval)
+                )
+                    .order_by(DbIndexBarData.datetime.asc())
+                    .first()
+            )
+            overview.start = start_bar.datetime
+
+            end_bar: DbIndexBarData = (
+                DbIndexBarData.select()
+                    .where(
+                    (DbIndexBarData.symbol == data.symbol)
+                    & (DbIndexBarData.exchange == data.exchange)
+                    & (DbIndexBarData.interval == data.interval)
+                )
+                    .order_by(DbIndexBarData.datetime.desc())
                     .first()
             )
             overview.end = end_bar.datetime
@@ -574,6 +874,20 @@ class MysqlDatabase(BaseDatabase):
         """查询数据库中的基础信息汇总信息"""
 
         s: ModelSelect = DbBasicStockData.select()
+        overviews = {}
+        for overview in s:
+            overview.exchange = Exchange(overview.exchange)
+            market = Market(overview.market)
+            overview.market = market
+            if market not in overviews:
+                overviews[market] = []
+            overviews[overview.market].append(overview)
+        return overviews
+
+    def get_basic_index_data(self) -> Dict[Market, List[BasicIndexData]]:
+        """查询数据库中的基础信息汇总信息"""
+
+        s: ModelSelect = DbBasicIndexData.select()
         overviews = {}
         for overview in s:
             overview.exchange = Exchange(overview.exchange)
